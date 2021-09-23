64d398ea45eedba04c544dd42d1bb80a942170de
heochristopher <christopher.heo23@sitechhs.com>
2021-09-23 12:31:06 -0400

    asdf

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
+function calculate() {
+  const m = Number(document.getElementById('slope-input').value)
+  const b = Number(document.getElementById('y-intercept-input').value)
+  const length = Number(document.getElementById('length-input').value)
 
-async function getImage() {
-  try {
-   
-      const data = await fetch("https://randomfox.ca/floof/")
-   
-    console.log(data.json());
-  } catch (error) {
-    console.log(error);
-    alert("oops");
+  const yValues = []
+  const xValues = []
+
+  for(let i =0; i <= length; i++) {
+    const yValue = m * i + b
+    yValues.push(yValue)
+    xValues.push(i)
   }
+  console.log(yValues, xValues)
+
+  xValues.forEach((x) => {
+    const li = document.createElement('li')
+    const value = document.createTextNode(x)
+    li.appendChild(value)
+    document.getElementById('x-values').appendChild(li)
+  })
+  yValues.forEach((y) => {
+    const li = document.createElement('li')
+    const value = document.createTextNode(y)
+    li.appendChild(value)
+    document.getElementById('y-values').appendChild(li)
+  })
+  
 }
 
-getImage()
+const submitBtn = document.getElementById('submit-btn')
+submitBtn.addEventListener('click', function() {
+  calculate()
+})
+
