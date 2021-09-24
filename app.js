c60054ab2bd622fb1df77d536198c8f1b576c560
heochristopher <christopher.heo23@sitechhs.com>
2021-09-24 12:28:18 -0400

    finished

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function calculate() {
   const m = Number(document.getElementById('slope-input').value)
   const b = Number(document.getElementById('y-intercept-input').value)
-  const length = Number(document.getElementById('length-input').value)
+  const length = Number(document.getElementById('length-input').value) - 1
 
   const yValues = []
   const xValues = []
@@ -11,25 +11,56 @@ function calculate() {
     yValues.push(yValue)
     xValues.push(i)
   }
-  console.log(yValues, xValues)
 
-  xValues.forEach((x) => {
-    const li = document.createElement('li')
-    const value = document.createTextNode(x)
-    li.appendChild(value)
-    document.getElementById('x-values').appendChild(li)
-  })
-  yValues.forEach((y) => {
-    const li = document.createElement('li')
-    const value = document.createTextNode(y)
-    li.appendChild(value)
-    document.getElementById('y-values').appendChild(li)
-  })
+  if(xValues.length === 0 || yValues.length === 0 || length === 'undefined') {
+    alert('Enter all values')
+  } else if(document.getElementById('x-values').childElementCount > 0) {
+    while( document.getElementById('x-values').firstChild) {
+      document.getElementById('x-values').removeChild(document.getElementById('x-values').firstChild)
+    }
+    while( document.getElementById('y-values').firstChild) {
+      document.getElementById('y-values').removeChild(document.getElementById('y-values').firstChild)
+    }
+    document.getElementById('x-values').innerHTML += "X Values"
+    xValues.forEach((x) => {
+      const li = document.createElement('li')
+      const value = document.createTextNode(x)
+      li.appendChild(value)
+      document.getElementById('x-values').appendChild(li)
+    })
+     
+    document.getElementById('y-values').innerHTML += "Y Values"
+    yValues.forEach((y) => {
+      const li = document.createElement('li')
+      const value = document.createTextNode(y)
+      li.appendChild(value)
+      document.getElementById('y-values').appendChild(li)
+    })
+  }
+    else {
+    document.getElementById('x-values').innerHTML += "X Values"
+    xValues.forEach((x) => {
+      const li = document.createElement('li')
+      const value = document.createTextNode(x)
+      li.appendChild(value)
+      document.getElementById('x-values').appendChild(li)
+    })
+     
+    document.getElementById('y-values').innerHTML += "Y Values"
+    yValues.forEach((y) => {
+      const li = document.createElement('li')
+      const value = document.createTextNode(y)
+      li.appendChild(value)
+      document.getElementById('y-values').appendChild(li)
+    })
+  }
+  
   
 }
 
 const submitBtn = document.getElementById('submit-btn')
 submitBtn.addEventListener('click', function() {
+  
   calculate()
 })
 
