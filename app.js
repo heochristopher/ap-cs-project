d09e4e1bc1c79172c978084f4c2c5d6bd693119a
heochristopher <christopher.heo23@sitechhs.com>
2021-09-29 12:28:36 -0400

    do two buttons
    we need two function calls
    use bool argument for slope calculation or point calculation

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
 function calculate() {
+  //user inputs
   const m = Number(document.getElementById('slope-input').value)
   const b = Number(document.getElementById('y-intercept-input').value)
   const length = Number(document.getElementById('length-input').value) - 1
 
+  //array for storing output values
   const yValues = []
   const xValues = []
 
+  //calculate output values
   for(let i =0; i <= length; i++) {
+    //y = mx + b
     const yValue = m * i + b
+    //push y value to the array
     yValues.push(yValue)
+    //push iterator as x to the array
     xValues.push(i)
   }
 
+  //alert if any input is left blank OR lenth is 0
   if(xValues.length === 0 || yValues.length === 0 || length === 'undefined') {
     alert('Enter all values')
-  } else if(document.getElementById('x-values').childElementCount > 0) {
+  }
+  //if there are values present on the page, replace them with new values
+   else if(document.getElementById('x-values').childElementCount > 0) {
+     //remove existing values
     while( document.getElementById('x-values').firstChild) {
       document.getElementById('x-values').removeChild(document.getElementById('x-values').firstChild)
     }
     while( document.getElementById('y-values').firstChild) {
       document.getElementById('y-values').removeChild(document.getElementById('y-values').firstChild)
     }
+    //add new values
     document.getElementById('x-values').innerHTML += "X Values"
     xValues.forEach((x) => {
       const li = document.createElement('li')
@@ -37,6 +48,7 @@ function calculate() {
       document.getElementById('y-values').appendChild(li)
     })
   }
+  //if no elements on the page, just add new values
     else {
     document.getElementById('x-values').innerHTML += "X Values"
     xValues.forEach((x) => {
@@ -59,7 +71,9 @@ function calculate() {
 }
 
 const submitBtn = document.getElementById('submit-btn')
+//when calculate button clicked, store user inputs and call function
 submitBtn.addEventListener('click', function() {
+  
   
   calculate()
 })
